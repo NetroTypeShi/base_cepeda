9cea4f85629ca7f04d17fe58a3e1378eb4d740ee
NetroTypeShi <andresmenduina@gmail.com>
2026-01-23 16:27:56 +0100

    table anttemp and door prototype ended

--- Assets/Scripts/InteractWithObjects.cs
+++ Assets/Scripts/InteractWithObjects.cs
@@ -1,65 +1,83 @@
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class InteractWithObjects : MonoBehaviour
 {
     [SerializeField] GameObject Key;
     [SerializeField] GameObject Door;
-    [SerializeField] public bool interactionWithKey = false;
-    [SerializeField] public bool interactionWithDoor = false;
-    [SerializeField] bool playerGotKey = false;
-    void Start()
-    {
 
-    }
+    public bool interactionWithKey = false;
+    public bool interactionWithDoor = false;
+
+    bool playerGotKey = false;
+    bool doorOpened = false;
+
+    [SerializeField] float doorOpenAngle = 90f;
+    [SerializeField] float doorSpeed = 2f;
+
     private void Update()
     {
-        if (interactionWithKey == true && Input.GetKeyDown(KeyCode.E))
+        if (interactionWithKey && Input.GetKeyDown(KeyCode.E))
         {
             GotKeyMessage();
             playerGotKey = true;
             Destroy(Key);
         }
-        if(interactionWithDoor == true && playerGotKey == true && Input.GetKeyDown(KeyCode.E))
+
+        if (interactionWithDoor && playerGotKey && Input.GetKeyDown(KeyCode.E) && !doorOpened)
         {
             OpenDoorMessage();
-            Destroy(Door);
+            StartCoroutine(OpenDoor());
         }
     }
+
+    IEnumerator OpenDoor()
+    {
+        doorOpened = true;
+
+        Quaternion startRotation = Door.transform.rotation;
+        Quaternion endRotation = Quaternion.Euler(
+            Door.transform.eulerAngles.x,
+            Door.transform.eulerAngles.y + doorOpenAngle,
+            Door.transform.eulerAngles.z
+        );
+
+        float t = 0f;
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime * doorSpeed;
+            Door.transform.rotation = Quaternion.Lerp(startRotation, endRotation, t);
+            yield return null;
+        }
+    }
+
     void GotKeyMessage()
     {
         print("llave obtenida");
     }
+
     void OpenDoorMessage()
     {
         print("puerta abierta");
     }
 
-
     private void OnTriggerEnter(Collider interactCollider)
     {
         if (interactCollider.CompareTag("InteractiveObject"))
-        {
             interactionWithKey = true;
-        }
-        if (interactCollider.CompareTag("Door"))
-        {
-            interactionWithDoor = true;
-        }
 
+        if (interactCollider.CompareTag("Door"))
+            interactionWithDoor = true;
     }
 
     private void OnTriggerExit(Collider interactCollider)
     {
         if (interactCollider.CompareTag("InteractiveObject"))
-        {
             interactionWithKey = false;
-        }
+
         if (interactCollider.CompareTag("Door"))
-        {
             interactionWithDoor = false;
-        }
     }
 }
+
