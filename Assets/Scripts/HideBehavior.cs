fa80a9dc305b6cfc81e6c54d2c387adbf1b8d496
NetroTypeShi <andresmenduina@gmail.com>
2026-01-26 12:04:35 +0100

    hide under the table completed

---
+++ Assets/Scripts/HideBehavior.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class HideBehavior : MonoBehaviour
+{
+    [SerializeField] Vector3 hideScale;
+    [SerializeField] Transform hidePosition;
+    [SerializeField] GameObject table;
+    [SerializeField] GameObject player;
+    Vector3 defaultPlayerPosition;
+    Vector3 defaultPlayerScale;
+    bool isHiding = false;
+    bool interactingWithTable = false; 
+    void Start()
+    {
+        defaultPlayerScale = player.transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ENTRAR A ESCONDERSE
+        if (interactingWithTable && !isHiding && Input.GetKeyDown(KeyCode.E))
+        {
+            isHiding = true;
+
+            defaultPlayerScale = player.transform.localScale;
+            defaultPlayerPosition = player.transform.position;
+
+            player.transform.localScale = hideScale;
+            player.transform.position = hidePosition.position;
+        }
+
+        // SALIR DEL ESCONDITE
+        else if (isHiding && Input.GetKeyDown(KeyCode.E))
+        {
+            isHiding = false;
+
+            player.transform.position = defaultPlayerPosition;
+            player.transform.localScale = defaultPlayerScale;
+        }
+
+        if ( interactingWithTable == false)
+        {
+            player.transform.localScale = defaultPlayerScale;
+        }
+    }
+    private void OnTriggerEnter(Collider interactCollider)
+    {
+        if (interactCollider.CompareTag("HideTable"))
+        {
+            interactingWithTable = true;
+        }
+    }
+    private void OnTriggerExit(Collider interactCollider)
+    {
+        if (interactCollider.CompareTag("HideTable"))
+        {
+            interactingWithTable = false;
+        }
+    }
+}
